b90030cf6352f55fdc5d265ab3a8305765ba94f6
Tim Deutschman <timdeu05@gmail.com>
2025-11-05 12:16:25 -0800

    Added Scene Manager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        switch (sceneIndex)
+        {
+            case 0:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+                break;
+            case 1:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+                break;
+            case 2:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("ResultsScreen");
+                break;
+            default:
+                Debug.LogWarning("Invalid scene index provided.");
+                break;
+        }
+    }
+}
